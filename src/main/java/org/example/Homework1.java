7dd8cc10190a7a98009697577114174f7128c0d0
Oksana.Rymaruk <oksanarym@gmail.com>
2023-07-18 18:03:26 -0400

    New pul pequest for dz-4

---
+++ src/main/java/org/example/Homework1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Homework1 {
+        public static void main(String[] args) {
+            Homework2 homework2 =new Homework2();
+            int returnValue = homework2.printSomeInt();
+            System.out.println("~~~" + returnValue +"~~~");
+        }
+    }
